859fda4067d849fc5e16f6fe4f02e37582580457
Andrejus <anba8005@gmail.com>
2019-06-13 18:20:41 +0300

    export default , tsconfig fix

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+import { NDIMediaStream } from './NDIMediaStream';
+import { NDIMediaTrack } from './NDIMediaTrack';
+import { NDIPeerConfiguration } from './NDIPeerConfiguration';
+import { RTCDataChannel } from './RTCDataChannel';
+import { RTCIceCandidate } from './RTCIceCandidate';
+import { RTCPeerConnection } from './RTCPeerConnection';
+import { RTCSessionDescription } from './RTCSessionDescription';
+import { WRTC } from './WRTC';
+export { NDIMediaStream, NDIMediaTrack, NDIPeerConfiguration, RTCDataChannel, RTCIceCandidate, RTCPeerConnection, RTCSessionDescription, WRTC, };
---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const NDIMediaStream_1 = require("./NDIMediaStream");
+exports.NDIMediaStream = NDIMediaStream_1.NDIMediaStream;
+const NDIMediaTrack_1 = require("./NDIMediaTrack");
+exports.NDIMediaTrack = NDIMediaTrack_1.NDIMediaTrack;
+const RTCDataChannel_1 = require("./RTCDataChannel");
+exports.RTCDataChannel = RTCDataChannel_1.RTCDataChannel;
+const RTCIceCandidate_1 = require("./RTCIceCandidate");
+exports.RTCIceCandidate = RTCIceCandidate_1.RTCIceCandidate;
+const RTCPeerConnection_1 = require("./RTCPeerConnection");
+exports.RTCPeerConnection = RTCPeerConnection_1.RTCPeerConnection;
+const RTCSessionDescription_1 = require("./RTCSessionDescription");
+exports.RTCSessionDescription = RTCSessionDescription_1.RTCSessionDescription;
+const WRTC_1 = require("./WRTC");
+exports.WRTC = WRTC_1.WRTC;
+//# sourceMappingURL=index.js.map
