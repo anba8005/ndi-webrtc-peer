fee9dcc9b91035355deb3e733af467385f068ecf
anba8005 <anba8005@gmail.com>
2020-03-02 22:25:09 +0200

    encoder/decoder configuration

--- dist/NDIPeerConfiguration.d.ts
+++ dist/NDIPeerConfiguration.d.ts
@@ -16,8 +16,16 @@ export interface NDIConfiguration {
     persistent?: boolean;
     outputMode?: 'default' | 'vertical' | 'square';
 }
+export interface DecoderConfiguration {
+    hardware: 'none' | 'vaapi';
+}
+export interface EncoderConfiguration {
+    hardware: 'none' | 'vaapi';
+}
 export interface NDIPeerConfiguration extends RTCConfiguration {
     ndi: NDIConfiguration;
     preview?: PreviewConfiguration;
     cpuAdaptation?: boolean;
+    decoder?: DecoderConfiguration;
+    encoder?: EncoderConfiguration;
 }
--- src/NDIPeerConfiguration.ts
+++ src/NDIPeerConfiguration.ts
@@ -18,8 +18,18 @@ export interface NDIConfiguration {
 	outputMode?: 'default' | 'vertical' | 'square';
 }
 
+export interface DecoderConfiguration {
+	hardware: 'none' | 'vaapi';
+}
+
+export interface EncoderConfiguration {
+	hardware: 'none' | 'vaapi';
+}
+
 export interface NDIPeerConfiguration extends RTCConfiguration {
 	ndi: NDIConfiguration;
 	preview?: PreviewConfiguration;
 	cpuAdaptation?: boolean;
+	decoder?: DecoderConfiguration;
+	encoder?: EncoderConfiguration;
 }
